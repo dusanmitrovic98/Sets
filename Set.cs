1245eb25b3897d51cb728437ade00dec0d59c2c2
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-16 19:48:18 +0200

    Updated IsSubsetOf method.

--- Set.cs
+++ Set.cs
@@ -101,7 +101,10 @@ public class Set<T>
     {
         foreach (T item in this.Values)
         {
+            if (!otherSet.Contains(item))
+            {
 
+            }
         }
 
         return true;
